53d8d6da4c9f5feced6b8fb68a7cf9e28c0039e9
sandoc2 <cesar.sandovalvilla@dell.com>
2022-11-19 13:30:33 -0800

    adding first POC

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Nav></Nav>
+  <router-view />
+ 
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Nav from "@/components/Nav.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Nav,
+  },
+};
 </script>
 
 <style lang="scss">
+
+
+body{
+  margin: 0;
+  margin-top:0;
+  background-color: #eeeeee;
+}
+
+@media screen and (min-width: 640px) {
+  body{
+    margin: 0 30px;
+  }
+}
+
+@media screen and (max-width: 640px) {
+ 
+}
+
+h1,h2,h3,h4,p{
+  margin:0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.sun-grid{
+  display: grid;
+  grid-template-columns:repeat(12, 1fr);
+}
+
+.sun-col-4{
+  grid-column:span 4;
+}
+.sun-col-6{
+  grid-column:span 6;
+}
+.sun-col-8{
+  grid-column:span 8;
+}
+.sun-u-txt--left{
+  text-align: left;
+}
+.sun-u-txt--right{
+  text-align: right;
+}
+.sun-wrapper{
+  padding:20px;
+}
+
+
+.sun-label{
+  font-size: 14px;
+}
+.sun-title{
+  font-size: 18px;
+}
+.sun-description{
+  font-size: 12px;
+}
+
+
+
 </style>
